fa611d110f662741447d9883122c9c905122517c
Ankur Sheel <ankur.sheel@gmail.com>
2017-09-25 16:09:28 +1300

    case 89 : Adding empty projects

---
+++ JobApplicationHelper.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JobApplicationHelper.Models
+{
+    public class Class1
+    {
+    }
+}
--- JobApplicationHelper.Web/Program.cs
+++ JobApplicationHelper.Web/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace JobApplicationHelper
+namespace JobApplicationHelper.Web
 {
     public class Program
     {
