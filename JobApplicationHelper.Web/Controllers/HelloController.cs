061ab74f15932031cff52daf234a0ce1f278c78c
Ankur Sheel <ankur.sheel@gmail.com>
2017-09-26 15:50:52 +1300

    case 92 : added hello controller

---
+++ JobApplicationHelper.Web/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JobApplicationHelper.Web.Controllers
+{
+    [Route("api/hello")]
+    public class HelloController : Controller
+    {
+        [Route("greet/{name}")]
+        public IActionResult Greet(string name)
+        {
+            return Ok($"Hello {name}");
+        }
+    }
+}
