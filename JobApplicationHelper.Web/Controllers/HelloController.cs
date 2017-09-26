640684dd4e227bea8b75474616ee92cb3b8f0334
Ankur Sheel <ankur.sheel@gmail.com>
2017-09-27 09:33:14 +1300

    case 92 : added api project and api controller which the web controller talks to

--- JobApplicationHelper.Web/Controllers/HelloController.cs
+++ JobApplicationHelper.Web/Controllers/HelloController.cs
@@ -1,16 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace JobApplicationHelper.Web.Controllers
 {
-    [Route("api/hello")]
+    [Route("hello")]
     public class HelloController : Controller
     {
-        [HttpGet("{name}")]
-        public IActionResult Greet(string name)
+        readonly HttpClient _client;
+        private readonly string _uri;
+
+        public HelloController()
         {
-            return Ok($"Hello {name}");
+            _uri = "http://localhost:4064/api/hello";
+            _client = new HttpClient {BaseAddress = new Uri(_uri)};
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> Greet(string name)
+        {
+            HttpResponseMessage responseMessage = await _client.GetAsync(_uri + $"/{name}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                return Ok(responseData);
+            }
+            return Ok("error");
         }
     }
 }
