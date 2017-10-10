14a6e4cc1285789965adad00786ff94356c7c65b
Ankur Sheel <ankur.sheel@gmail.com>
2017-10-10 17:29:22 +1300

    case 94 : refactored test

--- JobApplicationHelper.Web.IntegrationTests/HelloControllerTest.cs
+++ JobApplicationHelper.Web.IntegrationTests/HelloControllerTest.cs
@@ -3,34 +3,35 @@ using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Autofac;
-using Microsoft.Extensions.PlatformAbstractions;
-using Xunit;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.PlatformAbstractions;
+using Xunit;
 
 namespace JobApplicationHelper.Web.IntegrationTests
 {
     public class HelloControllerTest
     {
-        private readonly HttpClient _apiClient;
+        private readonly HttpClient _webClient;
 
         public HelloControllerTest()
         {
+            HttpClient apiClient;
             var containerBuilder = new ContainerBuilder();
             using (var container = containerBuilder.Build())
             {
                 using (var webHostScope = container.BeginLifetimeScope(builder => builder.RegisterType<Api.Startup>().AsSelf()))
                 {
-                    var fullPath = Path.GetFullPath(Path.Combine(PlatformServices.Default.Application.ApplicationBasePath,
-                                                                 "..",
-                                                                 "..",
-                                                                 "..",
-                                                                 "..",
-                                                                 "JobApplicationHelper.Api"));
+                    var fullPath = Path.GetFullPath(Path.Combine(PlatformServices.Default.Application.ApplicationBasePath
+                                                                 , ".."
+                                                                 , ".."
+                                                                 , ".."
+                                                                 , ".."
+                                                                 , "JobApplicationHelper.Api"));
+                    var factory = webHostScope.Resolve<Func<IHostingEnvironment, IConfiguration, Api.Startup>>();
                     var builder = new WebHostBuilder().UseKestrel()
                                                       .UseContentRoot(fullPath)
                                                       .UseEnvironment("Development")
@@ -39,31 +40,21 @@ namespace JobApplicationHelper.Web.IntegrationTests
                                                       {
                                                           var hostingEnv = provider.GetRequiredService<IHostingEnvironment>();
                                                           var config = provider.GetRequiredService<IConfiguration>();
-                                                          var factory =
-                                                              webHostScope.Resolve<Func<IHostingEnvironment, IConfiguration, Api.Startup>>();
                                                           return factory(hostingEnv, config);
                                                       }));
                     var testServer = new TestServer(builder);
-                    _apiClient = testServer.CreateClient();
+                    apiClient = testServer.CreateClient();
                 }
-            }
-        }
 
-        [Fact]
-        public async Task ShouldReturnCorrectResponseWithAGreeting()
-        {
-            // Arrange
-            var containerBuilder = new ContainerBuilder();
-            using (var container = containerBuilder.Build())
-            {
                 using (var webHostScope = container.BeginLifetimeScope(builder => builder.RegisterType<Startup>().AsSelf()))
                 {
-                    var fullPath = Path.GetFullPath(Path.Combine(PlatformServices.Default.Application.ApplicationBasePath,
-                                                                 "..",
-                                                                 "..",
-                                                                 "..",
-                                                                 "..",
-                                                                 "JobApplicationHelper.Web"));
+                    var fullPath = Path.GetFullPath(Path.Combine(PlatformServices.Default.Application.ApplicationBasePath
+                                                                 , ".."
+                                                                 , ".."
+                                                                 , ".."
+                                                                 , ".."
+                                                                 , "JobApplicationHelper.Web"));
+                    var factory = webHostScope.Resolve<Func<IHostingEnvironment, IConfiguration, Startup>>();
                     var builder = new WebHostBuilder().UseKestrel()
                                                       .UseContentRoot(fullPath)
                                                       .UseEnvironment("Development")
@@ -72,23 +63,28 @@ namespace JobApplicationHelper.Web.IntegrationTests
                                                       {
                                                           var hostingEnv = provider.GetRequiredService<IHostingEnvironment>();
                                                           var config = provider.GetRequiredService<IConfiguration>();
-                                                          var factory = webHostScope.Resolve<Func<IHostingEnvironment, IConfiguration, Startup>>();
                                                           return factory(hostingEnv, config);
                                                       }))
-                                                      .ConfigureServices(services => services.TryAddSingleton(_apiClient));
+                                                      .ConfigureServices(services => services.TryAddSingleton(apiClient));
 
                     var testServer = new TestServer(builder);
-                    var client = testServer.CreateClient();
-
-                    // Act
-                    var response = await client.GetAsync("/hello/ankur");
-
-                    // Assert
-                    response.EnsureSuccessStatusCode();
-                    var responseData = response.Content.ReadAsStringAsync().Result;
-                    Assert.Equal("Hello ankur", responseData);
+                    _webClient = testServer.CreateClient();
                 }
             }
+        }
+
+        [Fact]
+        public async Task ShouldReturnCorrectResponseWithAGreeting()
+        {
+            // Arrange
+
+            // Act
+            var response = await _webClient.GetAsync("/hello/ankur");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var responseData = response.Content.ReadAsStringAsync().Result;
+            Assert.Equal("Hello ankur", responseData);
         }
     }
 }
