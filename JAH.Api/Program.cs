752c18454975fd31d23ed29cf91f083edf73cc1a
Ankur Sheel <ankur.sheel@gmail.com>
2017-11-13 08:31:55 +1300

    case 94: fixing broken tests

--- JAH.Api/Program.cs
+++ JAH.Api/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Autofac;
+using JAH.Data;
+using JAH.Data.Interfaces;
+using JAH.Data.Repositories;
+using JAH.DomainModels;
+using JAH.Services.Interfaces;
+using JAH.Services.Services;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -14,6 +21,12 @@ namespace JAH.Api
         public static async Task Main(string[] args)
         {
             var builder = new ContainerBuilder();
+            builder.RegisterType<JobApplicationService>().As<IJobApplicationService>();
+            builder.RegisterType<JobApplicationRepository>().As<IRepository<JobApplication>>();
+            var options = new DbContextOptionsBuilder<JobApplicationDbContext>()
+                .UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = JobApplicationData; Trusted_Connection = True;")
+                .Options;
+            builder.RegisterType<JobApplicationDbContext>().As<JobApplicationDbContext>().WithParameter(new TypedParameter(typeof(DbContextOptions<JobApplicationDbContext>), options));
 
             using (var container = builder.Build())
             {
---
+++ JAH.Web/Controllers/JobApplicationController.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JAH.Web.Controllers
+{
+    [Route("jobApplication")]
+    public class JobApplicationController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public JobApplicationController(HttpClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            HttpResponseMessage responseMessage = await _client.GetAsync($"api/jobApplication");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                return Ok(responseData);
+            }
+
+            return Ok("error");
+        }
+    }
+}
