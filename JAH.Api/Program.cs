ecba076d5a42e77d3792cfbf9adfe7d756b25a1a
Ankur Sheel <ankur.sheel@gmail.com>
2018-04-08 16:00:40 +1200

    added db seeding for user

--- JAH.Api/Program.cs
+++ JAH.Api/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
-using System.Threading.Tasks;
+
 using Autofac;
+
+using JAH.Data;
+using JAH.Data.Entities;
+
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace JAH.Api
 {
@@ -14,11 +20,24 @@ namespace JAH.Api
         {
             try
             {
-                using (IContainer container = IOCBuilder.Build())
+                using (var container = IOCBuilder.Build())
                 {
-                    SeedDatabase(container);
+                    var host = BuildWebHost(container);
+                    using (var scope = host.Services.CreateScope())
+                    {
+                        var services = scope.ServiceProvider;
+                        try
+                        {
+                            SeedDatabase(services);
+                        }
+                        catch (Exception ex)
+                        {
+                            var logger = services.GetRequiredService<ILogger<Program>>();
+                            logger.LogError(ex, "An error occurred while seeding the database.");
+                        }
+                    }
 
-                    StartWebServerAsync(container).Wait();
+                    host.Run();
                 }
             }
             catch (Exception e)
@@ -28,36 +47,37 @@ namespace JAH.Api
             }
         }
 
-        private static void SeedDatabase(IContainer container)
+        private static IWebHost BuildWebHost(ILifetimeScope scope)
+        {
+            using (var webHostScope = scope.BeginLifetimeScope(builder => builder.RegisterType<Startup>().AsSelf()))
+            {
+                return WebHost.CreateDefaultBuilder().
+                    UseStartup<Startup>().
+                    ConfigureServices(services => services.AddTransient(provider =>
+                        {
+                            var hostingEnv = provider.GetRequiredService<IHostingEnvironment>();
+                            var config = provider.GetRequiredService<IConfiguration>();
+                            var factory = webHostScope.Resolve<Func<IHostingEnvironment, IConfiguration, Startup>>();
+                            return factory(hostingEnv, config);
+                        })).
+                    Build();
+            }
+        }
+
+        private static void SeedDatabase(IServiceProvider services)
         {
             try
             {
-                //var context = container.Resolve<JobApplicationDbContext>();
-                //var context = container.Resolve<JobApplicationDbContext>();
-                //DbInitializer.Initialize(context);
+                var context = services.GetRequiredService<JobApplicationDbContext>();
+                var userManager = services.GetRequiredService<UserManager<JobApplicationUser>>();
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                var dbInitializerLogger = services.GetRequiredService<ILogger<DbInitializer>>();
+                DbInitializer.Initialize(context, userManager, roleManager, dbInitializerLogger).Wait();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 throw;
-            }
-        }
-
-        private static async Task StartWebServerAsync(ILifetimeScope scope)
-        {
-            using (ILifetimeScope webHostScope = scope.BeginLifetimeScope(builder => builder.RegisterType<Startup>().AsSelf()))
-            {
-                await WebHost.CreateDefaultBuilder()
-                             .UseStartup<Startup>()
-                             .ConfigureServices(services => services.AddTransient(provider =>
-                             {
-                                 var hostingEnv = provider.GetRequiredService<IHostingEnvironment>();
-                                 var config = provider.GetRequiredService<IConfiguration>();
-                                 var factory = webHostScope.Resolve<Func<IHostingEnvironment, IConfiguration, Startup>>();
-                                 return factory(hostingEnv, config);
-                             }))
-                             .Build()
-                             .RunAsync();
             }
         }
     }
--- JAH.Data/DbInitializer.cs
+++ JAH.Data/DbInitializer.cs
@@ -1,41 +1,148 @@
 ﻿using System;
 using System.Linq;
+using System.Threading.Tasks;
+
 using JAH.Data.Entities;
 using JAH.DomainModels;
 
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
 namespace JAH.Data
 {
-    public static class DbInitializer
+    public class DbInitializer
     {
-        public static void Initialize(JobApplicationDbContext context)
+        public static async Task Initialize(JobApplicationDbContext context,
+                                            UserManager<JobApplicationUser> userManager,
+                                            RoleManager<IdentityRole> roleManager,
+                                            ILogger<DbInitializer> logger)
         {
             context.Database.EnsureCreated();
 
-            if (context.JobApplications.Any())
+            if (context.Users.Any())
             {
                 // database has been seeded
                 return;
             }
 
-            var jobApplications = new[]
-            {
-                new JobApplicationEntity { CompanyName = "Company 1", ApplicationDate = new DateTime(2017, 11, 13), CurrentStatus = Status.Rejected },
-                new JobApplicationEntity { CompanyName = "Company 2", ApplicationDate = new DateTime(2017, 11, 14), CurrentStatus = Status.Applied },
-                new JobApplicationEntity
-                {
-                    CompanyName = "Company 3",
-                    ApplicationDate = new DateTime(2017, 11, 14),
-                    CurrentStatus = Status.Interview
-                },
-                new JobApplicationEntity { CompanyName = "Company 4", ApplicationDate = new DateTime(2017, 10, 9), CurrentStatus = Status.Offer }
-            };
+            var user = await CreateDefaultUserAndRoleForApplication(userManager, roleManager, logger);
 
-            foreach (JobApplicationEntity jobApplication in jobApplications)
-            {
-                context.JobApplications.Add(jobApplication);
-            }
+            var jobApplication = new JobApplicationEntity {
+                                                              CompanyName = "Company 1",
+                                                              ApplicationDate = new DateTime(2017, 11, 13),
+                                                              CurrentStatus = Status.Applied,
+                                                              Owner = user
+                                                          };
+
+            context.JobApplications.Add(jobApplication);
 
             context.SaveChanges();
+        }
+
+        private static async Task AddDefaultRoleToDefaultUser(UserManager<JobApplicationUser> userManager,
+                                                              ILogger<DbInitializer> logger,
+                                                              string administratorRole,
+                                                              JobApplicationUser user)
+        {
+            logger.LogInformation($"Add default user 'admin' to role '{administratorRole}'");
+            var ir = await userManager.AddToRoleAsync(user, administratorRole);
+            if (ir.Succeeded)
+            {
+                logger.LogDebug($"Added the role '{administratorRole}' to default user 'admin' successfully");
+            }
+            else
+            {
+                var exception = new ApplicationException($"The role `{administratorRole}` cannot be set for the user 'admin'");
+                logger.LogError(exception, GetIdentityErrorsInCommaSeperatedList(ir));
+                throw exception;
+            }
+        }
+
+        private static async Task CreateDefaultAdministratorRole(RoleManager<IdentityRole> roleManager,
+                                                                 ILogger<DbInitializer> logger,
+                                                                 string administratorRole)
+        {
+            logger.LogInformation($"Create the role `{administratorRole}` for application");
+            var ir = await roleManager.CreateAsync(new IdentityRole(administratorRole));
+            if (ir.Succeeded)
+            {
+                logger.LogDebug($"Created the role `{administratorRole}` successfully");
+            }
+            else
+            {
+                var exception = new ApplicationException($"Default role `{administratorRole}` cannot be created");
+                logger.LogError(exception, GetIdentityErrorsInCommaSeperatedList(ir));
+                throw exception;
+            }
+        }
+
+        private static async Task<JobApplicationUser> CreateDefaultUser(UserManager<JobApplicationUser> userManager, ILogger<DbInitializer> logger)
+        {
+            logger.LogInformation("Create default user for application");
+            const string UserName = "admin";
+
+            var user = new JobApplicationUser(UserName);
+
+            var ir = await userManager.CreateAsync(user);
+            if (ir.Succeeded)
+            {
+                logger.LogDebug("Created default 'user' admin successfully");
+            }
+            else
+            {
+                var exception = new ApplicationException($"Default user 'admin' cannot be created");
+                logger.LogError(exception, GetIdentityErrorsInCommaSeperatedList(ir));
+                throw exception;
+            }
+
+            var createdUser = await userManager.FindByNameAsync(UserName);
+            return createdUser;
+        }
+
+        private static async Task<JobApplicationUser> CreateDefaultUserAndRoleForApplication(
+            UserManager<JobApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ILogger<DbInitializer> logger)
+        {
+            const string AdministratorRole = "Administrator";
+
+            await CreateDefaultAdministratorRole(roleManager, logger, AdministratorRole);
+            var user = await CreateDefaultUser(userManager, logger);
+            await SetPasswordForDefaultUser(userManager, logger, user);
+            await AddDefaultRoleToDefaultUser(userManager, logger, AdministratorRole, user);
+
+            return user;
+        }
+
+        private static string GetIdentityErrorsInCommaSeperatedList(IdentityResult ir)
+        {
+            string errors = null;
+            foreach (var identityError in ir.Errors)
+            {
+                errors += identityError.Description;
+                errors += ", ";
+            }
+
+            return errors;
+        }
+
+        private static async Task SetPasswordForDefaultUser(UserManager<JobApplicationUser> userManager,
+                                                            ILogger<DbInitializer> logger,
+                                                            JobApplicationUser user)
+        {
+            logger.LogInformation($"Set password for default user 'admin'");
+            const string password = "admin";
+            var ir = await userManager.AddPasswordAsync(user, password);
+            if (ir.Succeeded)
+            {
+                logger.LogTrace($"Set password `{password}` for default user 'admin' successfully");
+            }
+            else
+            {
+                var exception = new ApplicationException($"Password for the user 'admin' cannot be set");
+                logger.LogError(exception, GetIdentityErrorsInCommaSeperatedList(ir));
+                throw exception;
+            }
         }
     }
 }
